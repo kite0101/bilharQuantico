66741d479a9cd34cd055d7461bdedec0460e3e98
kite <kite0101@gmail.com>
2013-05-05 17:12:14 -0300

    Tirei alguns comentarios inuteis do projeto. Tb resolve um erro na arvore de projeto.

--- src/Init.java
+++ src/Init.java
@@ -9,25 +9,19 @@ import gui.PlotBilhar;
 
 public class Init {
 	public Init() throws InterruptedException{
-		//ollada
-		//fdafs
-		//FractalFlocoNeveAleatorio mesa=new FractalFlocoNeveAleatorio(234321L, 600, 50,420);
-		//MesaSimples mesa= new MesaSimples();
-		//MesaFourier mesa= new MesaFourier(234321L, 500, 560);
+		
 		MesaElipseDistorcido mesa= new MesaElipseDistorcido(1, 600,19L);
-		//FractalFlocoNeveAleatorio mesa=new FractalFlocoNeveAleatorio(934321L, 600, 50,420);
-		//Stadium std=new Stadium(800,700);
+
 								
 		Particula par= new Particula(0.2, 0.3, new Vetor(0.2, 0.3),mesa.getListaRetas());
-		//Particula par2=new Particula(300, 180, new Vetor(0.001000000001, 0.001),proc.getMatrixProcessada());
+		
 		PlotBilhar plot= new PlotBilhar(mesa.getListaRetas(), par);
 		
 		new Janela(plot);
 		int ite=0;
 		while(true){
 			Thread.sleep(10);
-			par.atuali_posi();
-			//par2.atualizaPosi2();				
+			par.atuali_posi();			
 			ite++;			
 		}
 		
